81e5df36d29a3eebea37c6173308f8733133c5d2
Rafid <rafid200220@gmail.com>
2025-01-30 02:21:11 +1030

    he

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -9,5 +9,28 @@
 /*   Updated: 2025/01/28 13:35:23 by rtahmid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
+void	ft_putnbr(int nb)
+{
+	long int	n;
+
+	n = nb;
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n *= -1;
+	}
+	if (n >= 0 && n <= 9)
+		ft_putchar(n + '0');
+	if (n > 9)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+}
--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -9,5 +9,27 @@
 /*   Updated: 2025/01/28 13:35:57 by rtahmid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+int	ft_atoi(char *str)
+{
+	int	sum;
+	int	i;
+	int	sign;
 
-
+	i = 0;
+	sum = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		++i;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		sum = (sum * 10) + (str[i] - '0');
+		i++;
+	}
+	return (sign * sum);
+}
